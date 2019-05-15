79d7b1cb93837a2f45326293a19e24ea95e5c103
selvanayaki <kselvanayaki87@gmail.com>
2019-05-14 23:18:07 -0700

    Chess pattern

---
+++ Chesspattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Chesspattern
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //print 8*8 pattern chess board
+            for (int i = 0; i < 8; i++)
+            {
+
+                for (int j = 0; j < 8; j++)
+                    if (j % 2 == i % 2)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write("O");
+                    }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
