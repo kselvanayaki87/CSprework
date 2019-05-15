de8e415d89ee196d70ca0403364e97c642fa1bf4
selvanayaki <kselvanayaki87@gmail.com>
2019-05-15 00:16:55 -0700

    Mod2 Lab3

---
+++ Mod2_Lab3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mod2_Lab3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /*for (int counter = 0; counter < 10; counter++)
+            {
+                //String interpolation
+                Console.WriteLine($"Counter is at: {counter}");
+                //normal printing
+                Console.WriteLine("Counter is at {0}", counter);
+            }*/
+
+            int outer;
+            int inner;
+
+            for (outer = 2; outer < 100; outer++)
+            {
+                for (inner = 2; inner <= (outer / inner); inner++)
+                    if ((outer % inner) == 0) break; // if factor found, not prime
+                if (inner > (outer / inner))
+                    Console.WriteLine("{0} is prime", outer);
+            }
+        }
+    }
+}
