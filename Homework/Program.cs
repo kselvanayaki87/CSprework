568866a33669d7f50ff7e8de9c7394b6345c7d00
selvanayaki <kselvanayaki87@gmail.com>
2019-05-15 01:02:40 -0700

    Homework

---
+++ Homework/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Validate if entered character is an alphabet and lower/upper case
+            Console.WriteLine("Enter a character: ");
+            char c = (char)Console.Read();
+            if (Char.IsLetter(c))
+            {
+                if (Char.IsLower(c))
+                {
+                    Console.WriteLine("\nThe character is lowercase.");
+                }
+                else
+                {
+                    Console.WriteLine("\nThe character is uppercase.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nThe character isn't an alphabetic character.");
+            }
+
+
+
+
+        }
+    }
+}
