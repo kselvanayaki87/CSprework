c9fab65b8f7153fc1fbaf1cccde8e4145a92f8a7
selvanayaki <kselvanayaki87@gmail.com>
2019-05-15 00:19:47 -0700

    Mod2 Lab4

---
+++ Mod2_Lab4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mod2_Lab4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /* //while loop
+             int n = 1;
+             while (n < 6)
+             {
+                 Console.WriteLine($"Current value of n is {n}");
+                // Console.WriteLine("Current value of n is {0}", n);//
+                 n++;
+             }*/
+
+
+            //do while loop
+            int x = 10;
+            do
+            {
+                Console.WriteLine("Current value of x is {0}", x);
+                x++;
+            } while (x >= 10);
+        }
+    }
+}
