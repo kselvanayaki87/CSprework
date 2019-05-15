fc271418f94d54383210b42d2852dc926693c94a
selvanayaki <kselvanayaki87@gmail.com>
2019-05-15 00:10:39 -0700

    Mod2 lab1

---
+++ Mod2_Lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Mod2_Lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer value and press enter");
+            int inputvalue = Int32.Parse(Console.ReadLine());
+            if(inputvalue % 2 == 0)
+            {
+                Console.WriteLine("Entered number is even");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is not even");
+            }
+        }
+    }
+}
