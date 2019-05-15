169cd0e71b3f4209493c96bcbb9d717ed0f91148
selvanayaki <kselvanayaki87@gmail.com>
2019-05-14 23:02:43 -0700

    Initial check-in of module Mod1_Lab1

---
+++ Mod1_Lab1/EmptyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Mod1_Lab1
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+    /*
+    class test
+    {
+        static void Main()
+        {
+            String firstName = Console.ReadLine();
+            //Console.ReadLine();
+            //Console.WriteLine("Enter FirstName");
+            Console.WriteLine("{0}", "Entered firstName is" + " " + firstName);
+        }
+    }
+    */
+}
---
+++ Mod1_Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mod1_Lab1
+{
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+            String firstName;
+            String lastName = "";
+            DateTime vbirthdate;
+            vbirthdate = new DateTime(2016, 7, 24);
+            firstName = "Viyan";
+            //lastName = "Yuvaraj";
+            lastName = Console.ReadLine();
+            //Console.WriteLine(firstName + " " + lastName);
+            Console.WriteLine($"Born on {vbirthdate}");
+            Console.WriteLine("{0} {1}", firstName, lastName + " " + "Birthday is " + vbirthdate); 
+
+            /* String firstName = "Tom";
+             String lastName = "Sawyer";
+             Console.WriteLine(firstName + " " + lastName);
+             Console.WriteLine("{0} {1}" , firstName, lastName); */
+        }
+
+    }
+}
